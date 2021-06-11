dbf3e2d737c17a295034a23fa51d8a548e154214
hudsonbubba <42046042+hudsonbubba@users.noreply.github.com>
2021-06-11 17:47:08 -0400

    linev1

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private bool isChained;
+    private LineRenderer lineRenderer;
+
+    private void Awake()
+    {
+        isChained = false;
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.widthMultiplier = 0.3f;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool GetIsChained()
+    {
+        return isChained;
+    }
+
+    public void SetIsChained(bool val)
+    {
+        isChained = val;
+    }
+
+    public void ChangeLine()
+    {
+
+    }
+}
