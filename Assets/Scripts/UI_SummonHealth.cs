1147c6fea0a68078256ea858932c3ad6c14e7bcc
niksnon <non.niki@gmail.com>
2021-06-12 10:05:34 -0700

    Merge branch 'main' of https://github.com/JeeroyLenkinz/GMTK2021 into main

--- Assets/Scripts/UI_SummonHealth.cs
+++ Assets/Scripts/UI_SummonHealth.cs
@@ -8,6 +8,10 @@ public class UI_SummonHealth : MonoBehaviour
 {
     [SerializeField]
     private FloatReference summonHealthSO;
+    [SerializeField]
+    private BoolReference isChanneling;
+    [SerializeField]
+    private BoolReference isMovingToGhost;
 
     private Image healthBar;
     private int imageWidth;
@@ -28,13 +32,19 @@ public class UI_SummonHealth : MonoBehaviour
         healthBar.enabled = true;
     }
 
-    public void e_StopChanneling()
+    public void e_disableHealthBar()
     {
-        healthBar.enabled = false;
+        if (!isChanneling.Value || isMovingToGhost.Value) {
+            healthBar.enabled = false;
+        }
     }
 
     public void e_ChangeHealthBar()
     {
-        healthBar.rectTransform.localScale = new Vector3(summonHealthSO.Value/100, 1f, 1f);
+        if (summonHealthSO.Value > 0) {
+            healthBar.rectTransform.localScale = new Vector3(summonHealthSO.Value/100, 1f, 1f);
+        } else {
+            e_disableHealthBar();
+        }
     }
 }
