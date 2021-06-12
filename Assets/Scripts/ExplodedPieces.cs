e6e9cca66ef6b3794a718d2a40fa56b1dd7ac57a
hudsonbubba <42046042+hudsonbubba@users.noreply.github.com>
2021-06-12 17:25:06 -0400

    fadePieces

--- Assets/Scripts/ExplodedPieces.cs
+++ Assets/Scripts/ExplodedPieces.cs
@@ -1,14 +1,42 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ExplodedPieces : MonoBehaviour
 {
-    float timeToDeHitBox = 0.1f;
+    bool firstFrame = true;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        StartCoroutine(fadeMeOutCoroutine());
+    }
+
+    private void LateUpdate()
+    {
+        firstFrame = false;
+    }
+
+    public void OnCollisionEnter2D(Collision collision)
+    {
+        if (collision.transform.tag == "Player" && firstFrame)
+        {
+            StartCoroutine(fadeMeOutNow());
+        }
+    }
+
+    private IEnumerator fadeMeOutNow()
+    {
+        Tween tween = GetComponent<SpriteRenderer>().DOFade(0f, 1f);
+        yield return new WaitForSeconds(tween.Duration());
+        Destroy(this.gameObject);
+    }
+
+    private IEnumerator fadeMeOutCoroutine()
+    {
+        yield return new WaitForSeconds(5);
+        Tween tween = GetComponent<SpriteRenderer>().DOFade(0f, 1f);
+        yield return new WaitForSeconds(tween.Duration());
+        Destroy(this.gameObject);
     }
 }
