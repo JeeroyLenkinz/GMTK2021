7c9fdc0b1485b32929268f1fdf57879812d2c7ee
JeeroyLenkinz <rdotsikas@gmail.com>
2021-06-12 23:49:34 -0400

    Working Compass and Wave Number event

--- Assets/Scripts/Compass.cs
+++ Assets/Scripts/Compass.cs
@@ -7,19 +7,32 @@ public class Compass : MonoBehaviour
 {
     [SerializeField]
     private BoolReference isSevered;
+    private SpriteRenderer sprite;
+    [SerializeField]
+    private GameObject human;
+    [SerializeField]
+    private GameObject ghost;
+    public float displayOffset;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        sprite = GetComponent<SpriteRenderer>();
+        sprite.enabled = false;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
-    }
-
-    public void e_enableCompass() {
-
+        if (isSevered.Value) {
+            sprite.enabled = true;
+            Vector2 humanPos = new Vector2(human.transform.position.x, human.transform.position.y + 1.5f);
+            Vector2 ghostPos = ghost.transform.position;
+            Vector2 direction = (ghostPos - humanPos).normalized;
+            Vector2 stretchedDirection = direction * displayOffset;
+            gameObject.transform.position = humanPos + new Vector2(stretchedDirection.x, stretchedDirection.y);
+            gameObject.transform.up = direction;
+        } else {
+            sprite.enabled = false;
+        }
     }
 }
