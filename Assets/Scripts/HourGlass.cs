7a2f9f7683423fc6f1ac2e323e774d5b8c63db23
hudsonbubba <42046042+hudsonbubba@users.noreply.github.com>
2021-06-13 01:22:31 -0400

    title boom boom

---
+++ Assets/Scripts/HourGlass.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using ScriptableObjectArchitecture;
+
+public class HourGlass : MonoBehaviour
+{
+    [SerializeField]
+    private FloatReference summonHealthSO;
+
+    public RectTransform hourglass;
+    public RectTransform sand;
+    public RectTransform startPos;
+    public RectTransform endPos;
+    public GameObject HourGlassHolder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void e_StartChanneling()
+    {
+        HourGlassHolder.SetActive(true);
+    }
+
+    public void e_disableHealthBar()
+    {
+        HourGlassHolder.SetActive(false);
+    }
+
+    public void e_ChangeHealthBar()
+    {
+        if (summonHealthSO.Value > 0)
+        {
+            //float normalizedMove = ((startPos.rect.position.y - endPos.rect.position.y) * summonHealthSO.Value) + endPos.rect.position.y;
+            sand.localPosition = new Vector3(sand.rect.position.x, (100 - summonHealthSO.Value)*-1);
+        }
+        else
+        {
+            e_disableHealthBar();
+        }
+    }
+}
--- Assets/Scripts/VFX/WaveText.cs
+++ Assets/Scripts/VFX/WaveText.cs
@@ -8,7 +8,12 @@ public class WaveText : MonoBehaviour
 {
 
     public TextMeshProUGUI text;
+    private AudioSource audio;
 
+    private void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+    }
     public void e_New_Wave_Text(int waveNum)
     {
         StartCoroutine(newWave(waveNum));
@@ -23,8 +28,10 @@ public class WaveText : MonoBehaviour
     {
         text.fontSize = 300;
         text.DOFade(1f, 0.15f);
+        audio.Play();
         text.text = "WAVE";
         yield return new WaitForSeconds(1f);
+        audio.Play();
         text.text = waveNum.ToString();
         yield return new WaitForSeconds(1f);
         text.DOFade(0f, 0.15f);
@@ -34,10 +41,13 @@ public class WaveText : MonoBehaviour
     {
         text.DOFade(1f, 0.15f);
         text.fontSize = 250;
+        audio.Play();
         text.text = "Survive";
         yield return new WaitForSeconds(1f);
+        audio.Play();
         text.text = 5.ToString();
         yield return new WaitForSeconds(1f);
+        audio.Play();
         text.text = "Waves";
         yield return new WaitForSeconds(1f);
         text.DOFade(0f, 0.15f);
