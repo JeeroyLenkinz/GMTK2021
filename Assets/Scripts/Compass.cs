46cffe791c1e28d7b9bfcce341595801436811f5
JeeroyLenkinz <rdotsikas@gmail.com>
2021-06-12 23:09:39 -0400

    Starting Compass

---
+++ Assets/Scripts/Compass.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ScriptableObjectArchitecture;
+
+public class Compass : MonoBehaviour
+{
+    [SerializeField]
+    private BoolReference isSevered;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void e_enableCompass() {
+
+    }
+}
