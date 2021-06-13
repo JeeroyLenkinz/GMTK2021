3a21275d2b40ec9640e8a13dbfb9988976bd77a6
hudsonbubba <42046042+hudsonbubba@users.noreply.github.com>
2021-06-13 12:32:19 -0400

    Lives

---
+++ Assets/Scripts/LivesUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivesUI : MonoBehaviour
+{
+
+    public GameObject[] livesImages;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(GameObject image in livesImages)
+        {
+            image.SetActive(true); ;
+        }
+    }
+
+
+    public void e_UpdateLivesUI(int lives)
+    {
+        if (lives == 2)
+        {
+            livesImages[2].SetActive(false);
+        }
+        else if (lives == 1)
+        {
+            livesImages[1].SetActive(false);
+        }
+        else if (lives == 0)
+        {
+            livesImages[0].SetActive(false);
+        }
+
+    }
+
+}
