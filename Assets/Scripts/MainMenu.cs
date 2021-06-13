628ed1a3a8f1914bf212bf0ee7431b4e365c39d8
hudsonbubba <42046042+hudsonbubba@users.noreply.github.com>
2021-06-13 13:23:44 -0400

    WinLossFade

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,25 +1,49 @@
-﻿using System.Collections;
+﻿using ScriptableObjectArchitecture;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using ScriptableObjectArchitecture;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    private GameEvent menuFadeOut;
+
     public void PlayGame()
     {
-        SceneManager.LoadScene("Main");
+        StartCoroutine(PlayGameCo());
+    }
 
+    private IEnumerator PlayGameCo()
+    {
+        menuFadeOut.Raise();
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("Main");
     }
 
     public void LoadTutorial()
     {
-        SceneManager.LoadScene("Tutorial");
+        StartCoroutine(TutorialCo());
 
     }
 
+    private IEnumerator TutorialCo()
+    {
+        menuFadeOut.Raise();
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("Tutorial");
+    }
 
     public void QuitToTitle()
     {
+        StartCoroutine(QuitTitleCo());
+    }
+
+    private IEnumerator QuitTitleCo()
+    {
+        menuFadeOut.Raise();
+        yield return new WaitForSeconds(2f);
         SceneManager.LoadScene("Title");
     }
 
@@ -29,4 +53,6 @@ public class MainMenu : MonoBehaviour
         Debug.Log("QUIT");
         Application.Quit();
     }
+
+
 }
---
+++ Assets/Scripts/VFX/Fader.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using ScriptableObjectArchitecture;
+using UnityEngine.SceneManagement;
+
+public class Fader : MonoBehaviour
+{
+
+    public Image blackness;
+
+    private void Awake()
+    {
+        if(SceneManager.GetActiveScene().ToString() != "Title")
+        {
+            e_Fade_In();
+        }
+    }
+
+    public void e_Fade_In()
+    {
+        StartCoroutine(fadeInCoroutine());
+    }
+
+    public void e_Fade_Out()
+    {
+        StartCoroutine(fadeOutCoroutine());
+    }
+
+    private IEnumerator fadeInCoroutine()
+    {
+        if(blackness.gameObject.activeInHierarchy == false)
+        {
+            blackness.gameObject.SetActive(true);
+        }
+        blackness.DOFade(0f, 1.5f);
+        yield return new WaitForSeconds(1.5f);
+        blackness.gameObject.SetActive(false);
+    }
+
+    private IEnumerator fadeOutCoroutine()
+    {
+        if (blackness.gameObject.activeInHierarchy == false)
+        {
+            blackness.gameObject.SetActive(true);
+        }
+        blackness.DOFade(1f, 1.5f);
+        yield return new WaitForSeconds(1.5f);
+    }
+
+}
